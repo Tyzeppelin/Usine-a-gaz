7c5639600153f3761b02eafaf4939501215f52d2
Tyzeppelin <boschet.francois@gmail.com>
2014-03-23 17:39:36 +0100

    Javadoc jusqu'a ecriture inclus

--- src/compilateur/Ecriture.java
+++ src/compilateur/Ecriture.java
@@ -35,7 +35,7 @@ public class Ecriture {
     }
      
     /**
-     * Methode stati de fermeture du stream f
+     * Methode static de fermeture du stream f
      * @param f : l'outputstream a fermer
      */
     public static void fermer(OutputStream f) {
@@ -53,40 +53,71 @@ public class Ecriture {
 	try {f.write(c);}
         catch(IOException e) {erreur(e);}
     }
-
+    
+    /**
+     * Methode d'ecriture d'un caractere sur la sortie std
+     * @param c : le caractere a ecrire
+     */
     public static void ecrireChar(char c) {ecrireChar(System.out,c);}
 
-
-
-    //écriture d'une chaîne, avec éventuel passage à la ligne 
-     
+    /**
+     * Methode d'ecriture d'une chaine de caracteres dans le fichier passe en parametre
+     * @param f : le fichier a ecrire
+     * @param s : la chaine a ecrire
+     */
     public static void ecrireString(OutputStream f,String s) {
 	try {for (int i=0;i<s.length();i++) f.write(s.charAt(i));}
         catch(IOException e) {erreur(e);}
     }
-
+    
+    /**
+     * Methode d'ecriture d'une chaine de caracteres sur la sortie std
+     * @param s : la chaine de caracteres a ecrire
+     */
     public static void ecrireString(String s) {
 	ecrireString(System.out,s);
     }
 
+    /**
+     * Methode d'ecriture d'une chaine de caracteres avec retour a la ligne dans le fichier passe en parametre
+     * @param f : le fichier a ecrire
+     * @param s : la chaine a ecrire
+     */
     public static void ecrireStringln(OutputStream f,String s) {
 	ecrireString(f,s+"\r\n");
     }
 
+    /**
+     * Methode d'ecriture d'une chaine de caracteres avec retour a la ligne sur la sortie std
+     * @param s : la chaine de caracteres a ecrire
+     */
     public static void ecrireStringln(String s) {
 	ecrireStringln(System.out,s);
     }
 
 
 
-    //écriture d'un entier avec formatage éventuel 
-    
+    /**
+     * Methode d'ecriture d'un entier dans le fichier passe en parametre
+     * @param f : le fichier a ecrire
+     * @param x : l'entier a ecrire
+     */
     public static void ecrireInt(OutputStream f,int x) {
 	ecrireString(f,Integer.toString(x));
     }
 
+    /**
+     * Methode d'ecriture d'un entier sur la sortie std
+     * @param x : l'entier a ecrire
+     */
     public static void ecrireInt(int x) {ecrireInt(System.out,x);}
     
+    /**
+     * Methode d'ecriture d'un entier dans le fichier passe en parametre avec la longueur en plus (?)
+     * @param f : le fichier a ecrire
+     * @param x : l'entier a ecrire
+     * @param longueur : la longueur
+     */
     public static void ecrireInt(OutputStream f,int x,int longueur) {
 	String s=Integer.toString(x);
 	int k=longueur-s.length();
@@ -94,20 +125,37 @@ public class Ecriture {
 	ecrireString(f,s);
     }
 
+    /**
+     * Methode d'ecriture d'un entier sur la sortie std avec la longueur en pluls (?)
+     * @param x : l'entier a ecrire
+     * @param longueur : la longueur
+     */
     public static void ecrireInt(int x,int longueur) {
 	ecrireInt(System.out,x,longueur);
     }
 
 
-
-    //écriture d'un double avec formatage éventuel                   
-  
+    /**
+     * Methode d'ecriture d'un double dans le fichier passe en parametre
+     * @param f : le fichier a ecrire
+     * @param s : le double a ecrire
+     */
     public static void ecrireDouble(OutputStream f,double d) {
 	ecrireString(f,Double.toString(d));
     }
 
+    /**
+     * Methode d'ecriture d'un double sur la sortie std
+     * @param d : le double a ecrire
+     */
     public static void ecrireDouble(double d) {ecrireDouble(System.out,d);}
 
+    /**
+     * Methode d'ecriture d'un double dans le fichier passe en parametre avec la longueur en plus (?)
+     * @param f : le fichier a ecrire
+     * @param d : le double a ecrire
+     * @param longueur : la longueur
+     */
     public static void ecrireDouble(OutputStream f,double d,int longueur) {
 	String s=Double.toString(d);
 	int k=longueur-s.length();
@@ -115,6 +163,11 @@ public class Ecriture {
 	ecrireString(f,s);
     }
 
+    /**
+     * Methode d'ecriture d'un double sur la sortie std avec la longueur en pluls (?)
+     * @param d : le double a ecrire
+     * @param longueur : la longueur
+     */
     public static void ecrireDouble(double d,int longueur) {
 	ecrireDouble(System.out,d,longueur);
     }
